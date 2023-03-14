ca9778d93f98a18d366ee79e2f8ac1edf7e90be1
Juan Chung <juanchung7v@gmail.com>
2023-03-13 20:09:00 -0700

    modified quiz.html

--- scripts/firebaseAPI_BBY27.js
+++ scripts/firebaseAPI_BBY27.js
@@ -7,7 +7,7 @@ var firebaseConfig = {
   projectId: "tempeture-de483",
   storageBucket: "tempeture-de483.appspot.com",
   messagingSenderId: "203803474565",
-  appId: "1:203803474565:web:508a7c9ee2fcf4f63847ec"
+  appId: "1:203803474565:web:508a7c9ee2fcf4f63847ec",
 };
 
 //--------------------------------------------
@@ -15,4 +15,5 @@ var firebaseConfig = {
 // initialize Firestore database if using it
 //--------------------------------------------
 const app = firebase.initializeApp(firebaseConfig);
-
+const db = firebase.firestore();
+const storage = firebase.storage();
--- scripts/quiz.js
+++ scripts/quiz.js
@@ -1,3 +1,41 @@
+function writeQuestions() {
+  var questionsRef = db.collection("questions");
+  quizzesRef.add({
+    id: "q001",
+    question: "I have often succeeded on a test or task even though I was afraid that I would not do well before I undertook the task.",
+    answer: [
+      { text: "1 (not all true)", points: 1 },
+      { text: "2 (rarely)", points: 2 },
+      { text: "3 (sometimes)", points: 3 },
+      { text: "4 (often)", points: 4 },
+      { text: "5 (very true)", points: 5 }
+    ],
+  });
+  quizzesRef.add({
+    id: "q002",
+    question: "I can give the impression that I’m more competent than I really am.",
+    answer: [
+      { text: "1 (not all true)", points: 1 },
+      { text: "2 (rarely)", points: 2 },
+      { text: "3 (sometimes)", points: 3 },
+      { text: "4 (often)", points: 4 },
+      { text: "5 (very true)", points: 5 }
+    ],
+  });
+  quizzesRef.add({
+    id: "q003",
+    question: "I avoid evaluations if possible and have a dread of others evaluating me.",
+    answer: [
+      { text: "1 (not all true)", points: 1 },
+      { text: "2 (rarely)", points: 2 },
+      { text: "3 (sometimes)", points: 3 },
+      { text: "4 (often)", points: 4 },
+      { text: "5 (very true)", points: 5 }
+    ],
+  });
+}
+
+
 function writeQuizzes() {
   //define a variable for the collection you want to create in Firestore to populate data
   var quizzesRef = db.collection("quizzes");
@@ -43,11 +81,16 @@ function writeQuizzes() {
 function displayQuizzesDynamically(collection) {
   let quizTemplate = document.getElementById("quizTemplate");
 
-  db.collection(collection)
+  db.collection("quizzes")
+    .limit(1)
     .get() //the collection called "quizzes"
-    .then((allQuizzes) => {
-      //var i = 1;  //Optional: if you want to have a unique ID for each hike
-      allQuizzes.forEach((doc) => {
+    .then((querySnapshot) => {
+      querySnapshot.forEach((doc) => {
+        console.log(doc.id, " => ", doc.data());
+        // .then((allQuizzes) => {
+        //var i = 1;  //Optional: if you want to have a unique ID for each hike
+        // allQuizzes.forEach((doc) => {
+
         //iterate thru each doc
         var quizId = doc.data().id; // get value of the "id" key
         var animal = doc.data().animal; // get value of the "animal" key
@@ -60,15 +103,17 @@ function displayQuizzesDynamically(collection) {
         var docID = doc.id;
         let templateClone = quizTemplate.content.cloneNode(true);
 
+        console.log(answer);
         //update title and text and image
         templateClone.querySelector(".question").innerHTML = question;
         templateClone.querySelector(".choice01").innerHTML = choice01;
         templateClone.querySelector(".choice02").innerHTML = choice02;
         templateClone.querySelector(".choice03").innerHTML = choice03;
         templateClone.querySelector(".choice04").innerHTML = choice04;
-        templateClone.querySelector(
-          ".animal-image"
-        ).src = `./images/${hikeCode}.jpg`; //Example: NV01.jpg
+
+        // templateClone.querySelector(
+        //   ".animal-image"
+        // ).src = `./images/${hikeCode}.jpg`; //Example: NV01.jpg
         //   templateClone.querySelector('a').href = "quiz.html?docID="+docID;
 
         //Optional: give unique ids to all elements for future use
@@ -77,9 +122,7 @@ function displayQuizzesDynamically(collection) {
         // newcard.querySelector('.card-image').setAttribute("id", "cimage" + i);
 
         //attach to gallery, Example: "hikes-go-here"
-        document
-          .getElementById(collection + "-go-here")
-          .appendChild(templateClone);
+        document.getElementById("answer-goes-here").appendChild(templateClone);
 
         //i++;   //Optional: iterate variable to serve as unique ID
       });
@@ -95,6 +138,7 @@ var modal = document.getElementById("rightAnswerModal");
 var backdrop = document.getElementById("backdrop");
 
 function openModal() {
+  console.log("working!");
   //   modal.classList.add("show");
   //   document.body.classList.add("modal-open");
   backdrop.style.display = "block";
@@ -115,3 +159,31 @@ window.onclick = function (event) {
     closeModal();
   }
 };
+
+//When the game starts add the user info
+function startGame() {
+  var gamesRef = db.collection("games");
+
+  gamesRef.add({
+    gameId: "0001", // gameID starts from 0001, next game + 1.
+    player1: "1234", // current login user's id (defauly player)
+    player1NickName: "dog",
+    player2: "2345", // an opponent (inivted friend)
+    player2NickName: "cat",
+    gameStatus: "active", //pending, active, done
+    cuurentQuestionId: "ab01",
+    currentQNum: "1",
+    maxQNum: "10",
+    statusP1: "unanswered",
+    statusP2: "unanswered",
+    scoreP1: "0",
+    scoreP2: "0",
+  });
+}
+
+// When you start the game above game data should be store in db(or local storage?)
+// The data is stored else where when the game is over. (archive)
+// The archive data can be used later when needed (leaderboard, profile)
+
+// I want this data base to be active only when player1 is playing game
+// It's not just data it's active data (it gets updated when the user chooses to do different actions)
