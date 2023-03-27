8c1d385eeb4086629f6cd3b9e55c207e53d868dd
Ruben Gill <rgill288@my.bcit.ca>
2023-03-26 17:41:55 -0700

    Created friends.js, which allows users to add friends. When they add a friend, it will get added to a subcollection under that user titled friend in firestore. Also wrote the function to read the data from firestore into the friend.html, so the user can see their currently added friends, but it does not yet work properly. Created friendbutton.html which the webpage gets dynamically loaded when a user searches for someone using their email, as AJAX is used.

--- scripts/friends.js
+++ scripts/friends.js
@@ -1,31 +1,108 @@
-// Get a reference to the current user's document
-const currentUserDocRef = firebase.firestore().collection('users').doc(currentUserID);
+// Add event listener to search button
+document.getElementById('search-button').addEventListener('click', function (event) {
+  event.preventDefault(); // prevent form submission
 
-// Search for the friend's user document by email address
-firebase.firestore().collection('users')
-  .where('email', '==', friendEmail)
-  .get()
-  .then((querySnapshot) => {
-    if (!querySnapshot.empty) {
-      // Found the friend's user document
-      const friendDocRef = querySnapshot.docs[0].ref;
+  // Get search query from input field
+  var searchQuery = document.getElementById('search-input').value;
 
-      // Add a new friend document to the friend's "friends" subcollection
-      friendDocRef.collection('friends').add({
-        name: 'Current User',
-        email: 'currentuser@example.com'
-      })
-      .then(() => {
-        console.log('Friend added successfully!');
-      })
-      .catch((error) => {
-        console.error('Error adding friend:', error);
-      });
-    } else {
-      // Friend's user document not found
-      console.log('Friend not found!');
-    }
-  })
-  .catch((error) => {
-    console.error('Error searching for friend:', error);
+  // Query Firestore for matching email data
+  db.collection('users').where('email', '==', searchQuery).get().then(function (querySnapshot) {
+    // Clear previous search results
+    document.getElementById('search-results').innerHTML = '';
+
+    // Load email template HTML using AJAX
+    var xhr = new XMLHttpRequest();
+    xhr.open('GET', 'text/friendbutton.html');
+    xhr.onload = function () {
+      if (xhr.status === 200) {
+        var emailHtml = xhr.responseText;
+
+        // Loop through matching email data and create new email cards
+        querySnapshot.forEach(function (doc) {
+          var userData = doc.data();
+          var userCard = document.createElement('div');
+          userCard.innerHTML = emailHtml;
+
+          // Set email card content
+          userCard.querySelector('.card-title').textContent = userData.name;
+          userCard.querySelector('.card-subtitle').textContent = userData.email;
+          userCard.querySelector('.card-text').textContent = userData.message;
+
+          // Add event listener to add friend button
+          var addFriendButton = userCard.querySelector('.add-friend-button');
+
+          // Retrieve the friend's email address and name
+          var friendEmail = userData.email;
+          var friendName = userData.name;
+
+          // Get the current user's document ID
+          var userId = firebase.auth().currentUser.uid;
+
+          // Query Firestore to check if the friend has already been added
+          db.collection('users').doc(userId).collection('friends').where('email', '==', friendEmail,).get().then(function (querySnapshot) {
+            if (querySnapshot.empty) {
+              // Friend has not been added, enable the add friend button
+              addFriendButton.addEventListener('click', function (event) {
+                event.preventDefault();
+                // Add friend to the 'friends' subcollection in firestore,
+                //so each user will have their own set of unique friends. 
+                db.collection('users').doc(userId).collection('friends').add({
+                  name: friendName,
+                  email: friendEmail,
+                  date_added: new Date()
+                }).then(function (docRef) {
+                  console.log('Friend added successfully!');
+                }).catch(function (error) {
+                  console.error('Error adding friend: ', error);
+                });
+              });
+            } else {
+              // Friend has already been added, gray out the add friend button
+              addFriendButton.disabled = true;
+              addFriendButton.textContent = 'Friend added';
+            }
+          });
+
+          // Add email card to search results
+          document.getElementById('search-results').appendChild(userCard);
+        });
+      } else {
+        console.log('Request failed.  Returned status of ' + xhr.status);
+      }
+    };
+    xhr.send();
   });
+});
+
+
+// document.addEventListener('DOMContentLoaded', function () {
+//   loadFriends();
+// });
+// function loadFriends() {
+//   const userId = firebase.auth().currentUser.uid;
+//   const friendsRef = firebase.firestore().collection("users").doc(userId).collection("friends");
+
+//   friendsRef.get().then(function (querySnapshot) {
+//     let friends = [];
+//     querySnapshot.forEach(function (doc) {
+//       let friend = doc.data();
+//       friend.id = doc.id;
+//       friends.push(friend);
+//     });
+//     console.log(friends); // log the friends array to the console
+//     let html = '';
+//     friends.forEach(function (friend) {
+//       html += '<div class="friendcard">';
+//       html += '<h2 class="friend-name">' + friend.name + '</h2>';
+//       html += '<p class="friend-email">' + friend.email + '</p>';
+//       html += '</div>';
+//     });
+//     document.getElementById('current-friends').innerHTML = html;
+//   });
+// }
+
+
+
+
+
+
